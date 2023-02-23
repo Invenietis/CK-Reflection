4ee02ae062cc41b6c41932df1d318f50c85f2245
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-02-23 19:14:06 +0100

    Bump dependencies.  (...) Updated new NUGET_ORG_PUSH_API_KEY.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -1,10 +1,6 @@
 using Cake.Common.IO;
-using Cake.Common.Solution;
 using Cake.Core;
 using Cake.Core.Diagnostics;
-using Cake.Core.IO;
-using SimpleGitVersion;
-using System.Linq;
 
 namespace CodeCake
 {
